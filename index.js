adc92b1bd574d254caf2574c3728515cd613a681
lando2319 <mikepland@gmail.com>
2023-04-01 09:10:08 -0500

    adds worldcities data, index.js comments and README

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("start")
+
+// get list of cities
+
+
+
+// Dev Process
+// - Clear Screen
+// - query city
+// - SHOW city on screen
+// - query for weather in city
+// - produce final query "Sunny in Chicago in 16 Grayscale"
+// - query dall-e OR google image
+// - send to WeatherWindow
+
+// STRETCH
+// - make photo public and somehow feed that to mpl.com
+// - So if I talk about it I can give a link so people can see the same image
+// - button on device to refresh
+
+
+console.log("starting process");
+
+
+
