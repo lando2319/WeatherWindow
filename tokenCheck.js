28e86f8c8299988a699440c095655d05eae98b38
lando2319 <mikepland@gmail.com>
2023-04-29 07:34:47 -0500

    adds spaces in links on tokenCheck, removes undefined.html, and overwrites testImagePage with stock photo

--- tokenCheck.js
+++ tokenCheck.js
@@ -9,19 +9,19 @@ function check() {
     if (!process.env.GOOGLE_PLACES_API_KEY) {
         missingPlaces = true;
         slugs.push("places");
-        tokenMessages.push("- Google Place API Key: need google console account with billing enabled, then see the MAPS API page at https://console.cloud.google.com/google/maps-apis/credentials\nNeeded to get Photo of Random Place")
+        tokenMessages.push("- Google Place API Key: need google console account with billing enabled, then see the MAPS API page at https://console.cloud.google.com/google/maps-apis/credentials \nNeeded to get Photo of Random Place")
     } 
     
     if (!process.env.OPENAI_API_KEY) {
         missingOpenAI = true;
         slugs.push("openai");
-        tokenMessages.push("- OpenAI Key: Signup and get Token at http://openai.com/\nNeeded for Dall-e Photo Generation")
+        tokenMessages.push("- OpenAI Key: Signup and get Token at http://openai.com/ \nNeeded for Dall-e Photo Generation")
     } 
     
     if (!process.env.PIRATE_WEATHER_API_KEY) {
         missingPirate = true;
         slugs.push("weather");
-        tokenMessages.push("- Pirate Weather: Get Token at https://pirate-weather.apiable.io/products/weather-data\nNeeded to get Weather Forecast")
+        tokenMessages.push("- Pirate Weather: Get Token at https://pirate-weather.apiable.io/products/weather-data \nNeeded to get Weather Forecast")
     }
 
     if (missingPlaces || missingPirate || missingPirate) {
