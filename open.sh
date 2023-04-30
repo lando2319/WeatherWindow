36b54c3a776fa2db65918334a5a7aa475accf48d
lando2319 <mikepland@gmail.com>
2023-04-30 05:22:08 -0500

    chains the index and the open into one command on start.sh, just one call now

--- open.sh
+++
@@ -1,1 +0,0 @@
-chromium-browser --display=:0 /home/lando2319/WeatherWindow/home.html
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-node /home/lando2319/WeatherWindow/index.js
+node /home/lando2319/WeatherWindow/index.js && 
+chromium-browser --display=:0 /home/lando2319/WeatherWindow/home.html
