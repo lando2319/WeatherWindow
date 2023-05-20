4bce8bb44b8ac39113c18c724ae95305fc7392dd
lando2319 <mikepland@gmail.com>
2023-05-20 18:32:38 -0500

    adds --disabled-extensions flag to start.sh this clears out warning and opens as expected

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 node /home/lando2319/WeatherWindow/index.js && 
-chromium-browser --kiosk --display=:0 /home/lando2319/WeatherWindow/home.html
+chromium-browser --kiosk --disabled-extensions --display=:0 /home/lando2319/WeatherWindow/home.html
