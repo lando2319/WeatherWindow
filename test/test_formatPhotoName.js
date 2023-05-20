8ff6e2e90f5692d9434e9a4a4c08979f7fa5d6ab
lando2319 <mikepland@gmail.com>
2023-05-19 21:40:58 -0500

    removes old test for checking for previous post since the new query approach does it directly, updates format photo name test to new pwd and file name and function name, passes test

--- test/test_formatPhotoName.js
+++ test/test_formatPhotoName.js
@@ -1,12 +1,12 @@
 
 var assert = require('assert');
-var formatName = require("../utility/formatPhotoName.js");
+var formatName = require("../utility/nameFormatterTool.js");
 
 describe("Testing Formatting Photo Name", function() {
     it('with and incoming name of Cloudy weather in Paris France in Black and White, should turn into cloudy-weather-in-paris-france-in-black-and-white-1683684313857.png', function() {
         var query = "Cloudy weather in Paris France in Black and White";
         var rightNow = Date.now();
-        var formattedName = formatName.run(query, rightNow);
+        var formattedName = formatName.format(query, rightNow);
 
         assert.equal(formattedName, "cloudy-weather-in-paris-france-in-black-and-white-" + rightNow + ".png");
     })
