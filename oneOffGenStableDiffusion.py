73166de8e93c99e584014b5326e2cce81f118c57
MIKE LAND <mikepland@gmail.com>
2023-06-24 05:17:20 -0500

    adds additional vars to .envExample, comments out progress bar hiding, ups steps on oneOffGenStableDiffusion, adds one off image

--- oneOffGenStableDiffusion.py
+++ oneOffGenStableDiffusion.py
@@ -5,12 +5,12 @@ from diffusers import DiffusionPipeline
 pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1")
 pipe = pipe.to("mps")
 
-pipe.set_progress_bar_config(disable=True)
+# pipe.set_progress_bar_config(disable=True)
 pipe.enable_attention_slicing()
 
 print("Starting Process")
 
-steps = 50
+steps = 20
 query = "Stormy Weather in Monte Carlo"
 
 image = pipe(query, num_inference_steps=steps).images[0]
