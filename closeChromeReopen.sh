150e03d76541da40296a1be3183f26c61d32cfb2
MIKE LAND <mikepland@gmail.com>
2023-06-27 11:42:38 -0500

    removes old test files and adds closeChromeReopen

---
+++ closeChromeReopen.sh
@@ -0,0 +1,1 @@
+killall "Google Chrome" && echo "Killed Chrome Processes" && sleep 4 && open -a "Google Chrome" --args --start-fullscreen --kiosk /Users/mikeland/WeatherWindow/home.html 
--- killChrome.sh
+++
@@ -1,2 +0,0 @@
-killall "Google Chrome" && open -a "Google Chrome" --args --start-fullscreen /Users/mikeland/WeatherWindow/home.html &&
-echo "Killed Chrome Processes"
