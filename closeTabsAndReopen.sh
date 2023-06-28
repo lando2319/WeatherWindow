74b32dcfa6b6dd7ac9a713e3adc3f0faa83b14a4
MIKE LAND <mikepland@gmail.com>
2023-06-27 20:25:07 -0500

    removes unneeded files, moves both crontab backups to new dir

--- closeTabsAndReopen.sh
+++
@@ -1,1 +0,0 @@
-sudo killall chromium-browser && chromium-browser --kiosk --disable-extensions /home/lando2319/WeatherWindow/home.html
--- start.sh
+++
@@ -1,2 +0,0 @@
-node /home/lando2319/WeatherWindow/index.js && 
-chromium-browser --kiosk --disabled-extensions --display=:0 /home/lando2319/WeatherWindow/home.html
