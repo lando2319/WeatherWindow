c162113a0d885964c872b8d94fcb6b8bd32e99af
MIKE LAND <mikepland@gmail.com>
2023-06-27 20:38:15 -0500

    puts close chrome and reopen steps on separate lines

--- closeChromeReopen.sh
+++ closeChromeReopen.sh
@@ -1,1 +1,5 @@
-killall "Google Chrome" && echo "Killed Chrome Processes" && sleep 4 && open -a "Google Chrome" --args --start-fullscreen --kiosk /Users/mikeland/WeatherWindow/home.html 
+killall "Google Chrome"
+echo "Killed Chrome Processes"
+sleep 4
+open -a "Google Chrome" --args --start-fullscreen --kiosk /Users/mikeland/WeatherWindow/home.html 
+echo "Reopend Chrome with home.html"
