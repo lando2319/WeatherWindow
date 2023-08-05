73b7e08ef79a4fb1145118c7678bba33341797de
lando2319 <mikepland@gmail.com>
2023-08-04 22:46:39 -0500

    remove extra comments on setQuery and adds more spices

--- utility/spiceUpMyQuery.js
+++ utility/spiceUpMyQuery.js
@@ -5,9 +5,18 @@ function spiceThis(query, spiceRating) {
         "in Black and White",
         "in line art style",
         "in hatching style",
-        "splottchy style",
+        "in splottchy style",
+        "in cinematic lighting style",
+        "in black and white linocut vector style",
+        "in soft tonal style",
+        "in kodak potra style",
+        "in 1980 cinematography, vintage, film grain style",
+        "in octane render style",
+        "in neo-noir style",
+        "in anime retro nostalgia style",
+        "in bight and bold in contrast style",
+        "in manga style"
     ]
-
 
     const spiceyRandom = Math.floor(Math.random() * 9) + 1;
     console.log("Random Number is", spiceyRandom);
